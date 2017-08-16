5f5bc3f4fc23823b1f7103235f2b87526586574a
MullerToSuccess <409601520@qq.com>
2017-08-16 17:52:10 +0800

    book module

--- app.js
+++ app.js
@@ -65,9 +65,9 @@ app.use('/analyse', analyse);
 app.use('/post/update', update);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
